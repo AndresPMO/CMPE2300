12bb7911e25ec162b00f9b27385f8994453105b9
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-09-27 15:20:35 -0600

    UI designed

--- ICA/ICA04_NicW/ICA04_NicW/Form1.Designer.cs
+++ ICA/ICA04_NicW/ICA04_NicW/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UI_button_Add = new System.Windows.Forms.Button();
+            this.UI_progressBar_Discard = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // UI_button_Add
+            // 
+            this.UI_button_Add.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_button_Add.Location = new System.Drawing.Point(13, 13);
+            this.UI_button_Add.Name = "UI_button_Add";
+            this.UI_button_Add.Size = new System.Drawing.Size(331, 64);
+            this.UI_button_Add.TabIndex = 0;
+            this.UI_button_Add.Text = "Add Blocks: Size - 80";
+            this.UI_button_Add.UseVisualStyleBackColor = true;
+            // 
+            // UI_progressBar_Discard
+            // 
+            this.UI_progressBar_Discard.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_progressBar_Discard.Location = new System.Drawing.Point(13, 84);
+            this.UI_progressBar_Discard.Name = "UI_progressBar_Discard";
+            this.UI_progressBar_Discard.Size = new System.Drawing.Size(331, 23);
+            this.UI_progressBar_Discard.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 114);
+            this.Controls.Add(this.UI_progressBar_Discard);
+            this.Controls.Add(this.UI_button_Add);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button UI_button_Add;
+        private System.Windows.Forms.ProgressBar UI_progressBar_Discard;
     }
 }
 
