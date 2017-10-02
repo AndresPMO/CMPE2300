11d05bf642df54afea7d4d59d8e696ef5e8def20
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-10-02 09:29:29 -0600

    Constructors done

--- ICA/ICA05_NicW/ICA05_NicW/Ball.cs
+++ ICA/ICA05_NicW/ICA05_NicW/Ball.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
 
 namespace ICA05_NicW
 {
+    public enum ESortType { eRadius, eDistance, eColour }
+
     class Ball
     {
+        //Static members
+        static private CDrawer canvas;
+        static private Random randNum;
+        static public ESortType sort { get; set; }
+
+        //Instance members
+        private float _radius;
+        private float Radius {
+            //Positive & !0
+            set {
+                if (value == 0)
+                    _radius = 1;
+                else
+                    _radius = Math.Abs(value);
+            }
+        }
+
+        private Color _colour;
+        private PointF _center;
+        
+        //Static constuctor
+        static Ball()
+        {
+            randNum = new Random();
+            canvas = new CDrawer(800, 600, false);
+            canvas.BBColour = Color.White;
+        }
+        //Instance constructor
+        public Ball(int inRadius)
+        {
+            Radius = inRadius;
+            _colour = RandColor.GetColor();
+            _center = new PointF(randNum.Next(_radius, canvas.ScaledWidth - _radius), randNum.Next(_radius, canvas.ScaledHeight - _radius));
+        }
+
+        //Static methods
+        //Instance methods
     }
 }
--- ICA/ICA05_NicW/ICA05_NicW/Form1.cs
+++ ICA/ICA05_NicW/ICA05_NicW/Form1.cs
@@ -7,9 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDIDrawer;
 
 namespace ICA05_NicW
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
