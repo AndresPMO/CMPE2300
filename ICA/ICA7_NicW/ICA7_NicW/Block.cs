69442a317813aa58d462cc54e1cd427fdcd035e7
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-10-12 08:18:14 -0600

    Variables done

---
+++ ICA/ICA7_NicW/ICA7_NicW/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICA7_NicW
+{
+    class Block
+    {
+        //static variables
+        public static CDrawer canvas { get; private set; }
+        public static int height { get; private set; }
+        private static Random randNum = new Random(1);
+
+        //instance variables
+        public int width { get;  private set; }
+        public bool Highlight { get; private set; }
+        private Color colour;
+    }
+}
