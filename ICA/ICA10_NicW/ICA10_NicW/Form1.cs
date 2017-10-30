9525a445dad67a4ff360564f9ef806231ac47027
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-10-30 09:52:55 -0600

    Done technically

--- ICA/ICA10_NicW/ICA10_NicW/Form1.cs
+++ ICA/ICA10_NicW/ICA10_NicW/Form1.cs
@@ -14,7 +14,7 @@ namespace ICA10_NicW
     public partial class Form1 : Form
     {
         static Random randNum = new Random();
-        List<Point> list = new List<Point>();
+        List<Point> listPoint = new List<Point>();
         LinkedList<Point> Linked = new LinkedList<Point>();
         CDrawer canvas = new CDrawer(800, 600, false);
 
@@ -29,43 +29,53 @@ namespace ICA10_NicW
             int divisor = (int)UI_numericUpDown_Divisor.Value;
 
             //Clear our current points
-            list.Clear();
+            listPoint.Clear();
 
             //Go through all the x values for every y value (X then Y)
             for(int y = 0; y < canvas.ScaledHeight; y += divisor)
             {
                 for(int x = 0; x < canvas.ScaledWidth; x += divisor)
                 {
-                    list.Add(new Point(x, y));
+                    listPoint.Add(new Point(x, y));
                 }
             }
 
             //All but the last in the list
             //Draw a fuchsia line to the next point
             canvas.Clear();
-            for(int i = 0; i < list.Count - 1; i++)
+            for(int i = 0; i < listPoint.Count - 1; i++)
             {
-                canvas.AddLine(list[i].X, list[i].Y, list[i + 1].X, list[i + 1].Y, Color.Fuchsia, 1);
+                canvas.AddLine(listPoint[i].X, listPoint[i].Y, listPoint[i + 1].X, listPoint[i + 1].Y, Color.Fuchsia, 1);
             }
             canvas.Render();
 
-            UI_button_MakeList.Text = $"List contains: {list.Count} points";
+            UI_button_MakeList.Text = $"List contains: {listPoint.Count} points";
         }
 
         private void UI_button_Shuffle_Click(object sender, EventArgs e)
         {
             //Need something in our list
-            if (list.Count == 0) return;
+            if (listPoint.Count == 0) return;
 
             //Do a shuffle!
-
+            Point temp;
+            for(int i = listPoint.Count -1; i > 1; i--)
+            {
+                int j = randNum.Next(0, i + 1); // 0 <= j <= i
+                //Hold onto i
+                temp = listPoint[i];
+                //swap j to i
+                listPoint[i] = listPoint[j];
+                //swap i to j
+                listPoint[j] = temp;
+            }
 
             //All but the last in the list
             //Draw a fuchsia line to the next point
             canvas.Clear();
-            for (int i = 0; i < list.Count - 1; i++)
+            for (int i = 0; i < listPoint.Count - 1; i++)
             {
-                canvas.AddLine(list[i].X, list[i].Y, list[i + 1].X, list[i + 1].Y, Color.Fuchsia, 1);
+                canvas.AddLine(listPoint[i].X, listPoint[i].Y, listPoint[i + 1].X, listPoint[i + 1].Y, Color.Green, 1);
             }
             canvas.Render();
         }
@@ -73,10 +83,44 @@ namespace ICA10_NicW
         private void UI_button_LinkedList_Click(object sender, EventArgs e)
         {
             //Need something in our list
-            if (list.Count == 0) return;
+            if (listPoint.Count == 0) return;
 
             //Do a sort!
+            Linked.Clear();
+            foreach(Point p in listPoint)
+            {
+                if(Linked.Count == 0)
+                {
+                    //First item to add
+                    Linked.AddFirst(p);
+                }
+                else
+                {
+                    //Need to find where to put it
+                    LinkedListNode<Point> sort = Linked.First;
+                    int sortKey = p.X * canvas.ScaledHeight + p.Y; //Given formula
+                    int currentValue = sort.Value.X * canvas.ScaledHeight + sort.Value.Y;
 
+                    while(currentValue < sortKey && sort != null)
+                    {
+                        //Change sort
+                        sort = sort.Next;
+                        if (sort != null)
+                        {
+                            currentValue = sort.Value.X * canvas.ScaledHeight + sort.Value.Y;
+                        }
+                    }
+
+                    if (sort == null)
+                    {
+                        Linked.AddLast(p);
+                    }
+                    else
+                    {
+                        Linked.AddBefore(sort, p);
+                    }
+                }
+            }
 
             //Draw some lines!
             LinkedListNode<Point> current = Linked.First;
@@ -88,6 +132,8 @@ namespace ICA10_NicW
                 current = current.Next;
             }
             canvas.Render();
+
+            UI_button_LinkedList.Text = $"LinkedList contains: {Linked.Count} points";
         }
     }
 }
