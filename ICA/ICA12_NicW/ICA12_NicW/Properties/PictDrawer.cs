a3340b1a93217fd25dbb12432104b49d8f8bbf16
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-13 09:33:19 -0700

    Added resource and code

---
+++ ICA/ICA12_NicW/ICA12_NicW/Properties/PictDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICA12_NicW.Properties
+{
+    class PictDrawer : CDrawer
+    {
+        Bitmap background = new Bitmap(Properties.Resources.gunFish);
+
+        public PictDrawer()
+        {
+
+        }
+    }
+}
