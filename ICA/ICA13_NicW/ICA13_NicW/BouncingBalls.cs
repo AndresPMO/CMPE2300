0e931e571b5812c74dbbdcb01e088da2db0e73c4
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-15 14:43:35 -0700

    Added ICA 8 base to ICA 13

---
+++ ICA/ICA13_NicW/ICA13_NicW/BouncingBalls.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace ICA13_NicW
+{
+    class BouncingBalls
+    {
+        //Static variables
+        static private Random randNum = new Random();
+
+        //Instance variables
+        private PointF center;
+        private float xVelocity;
+        private float yVelocity;
+        private int radius;
+        public Color colour { get; set; }
+
+        //Instance Constructor
+        public BouncingBalls(PointF inPoint, Color inColour)
+        {
+            center = inPoint;
+            colour = inColour;
+            xVelocity = (float)randNum.NextDouble() * 10 - 5;
+            yVelocity = (float)randNum.NextDouble() * 10 - 5;
+            radius = randNum.Next(20, 51);
+        }
+
+        //Overrides
+        public override bool Equals(object obj)
+        {
+            if (!(obj is BouncingBalls)) return false; //Null, or not ball type
+
+            BouncingBalls input = obj as BouncingBalls; //Get the input as type ball
+            float distance = this.GetDistance(input); //Find the distance between the two balls
+
+            return distance <= this.radius + input.radius; //If the distance is less than either balls radius, we have overlap
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        //Methods
+        private float GetDistance(BouncingBalls other)
+        {
+            return (float)Math.Abs(Math.Sqrt(Math.Pow((other.center.X - this.center.X), 2) + Math.Pow(other.center.Y - this.center.Y, 2))); //|Sqrt((x2-x1)^2 + (y2-y1)^2)|
+        }
+
+        public void ShowBall(CDrawer canvas, int num)
+        {
+            //Draw the circle
+            canvas.AddCenteredEllipse((int)center.X, (int)center.Y, (int)(radius * 2), (int)(radius * 2), colour);
+            //Add a number to the center of the ball
+            canvas.AddText(num.ToString(), 14, (int)center.X - radius, (int)center.Y - radius, radius * 2, radius * 2, Color.FromArgb(colour.ToArgb() ^ 0x00FFFFFF));
+        }
+
+        public void MoveBall(CDrawer canvas)
+        {
+            //If we have left the horizontal screen
+            if (center.X - radius < 0)
+            {
+                center.X = radius;
+            }
+            else if (center.X + radius > canvas.ScaledWidth)
+            {
+                center.X = canvas.ScaledWidth - radius;
+            }
+            //If we have left the vertical screen
+            if (center.Y - radius < 0)
+            {
+                center.Y = radius;
+            }
+            else if (center.Y + radius > canvas.ScaledHeight)
+            {
+                center.Y = canvas.ScaledHeight - radius;
+            }
+
+            //If we would leave the horizontal screen
+            if (xVelocity - radius + center.X < 0 || xVelocity + radius + center.X > canvas.ScaledWidth)
+            {
+                xVelocity *= -1;
+            }
+            //If we would leave the vertical screen
+            if (yVelocity - radius + center.Y < 0 || yVelocity + radius + center.Y > canvas.ScaledHeight)
+            {
+                yVelocity *= -1;
+            }
+
+            //Move
+            center.X += xVelocity;
+            center.Y += yVelocity;
+        }
+    }
+}
--- ICA/ICA13_NicW/ICA13_NicW/Form1.cs
+++ ICA/ICA13_NicW/ICA13_NicW/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDIDrawer;
 
 namespace ICA13_NicW
 {
     public partial class Form1 : Form
     {
+        List<BouncingBalls> greenList = new List<BouncingBalls>();
+        List<BouncingBalls> blueList = new List<BouncingBalls>();
+        List<BouncingBalls> redList = new List<BouncingBalls>();
+
+        CDrawer bgCanvas;
+        CDrawer rCanvas;
+
         public Form1()
         {
             InitializeComponent();
+            bgCanvas = new CDrawer(600, 300, false);
+            rCanvas = new CDrawer(600, 300, false);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Left click, add a green ball
+            if (bgCanvas.GetLastMouseLeftClickScaled(out Point lclick))
+            {
+                BouncingBalls temp = new BouncingBalls(lclick, Color.Green);
+                if (!greenList.Contains(temp))
+                {
+                    greenList.Add(temp);
+                }
+            }
+            //Right click, add a blue ball
+            if (bgCanvas.GetLastMouseRightClickScaled(out Point rclick))
+            {
+                BouncingBalls temp = new BouncingBalls(rclick, Color.Blue);
+                if (blueList.IndexOf(temp) < 0)
+                {
+                    blueList.Insert(0, temp);
+                }
+            }
+
+            //Move all the balls
+            greenList.ForEach(o => o.MoveBall(bgCanvas));
+            blueList.ForEach(o => o.MoveBall(bgCanvas));
+            redList.ForEach(o => o.MoveBall(bgCanvas));
+
+            //Check collisions
+            List<BouncingBalls> templist = greenList.Intersect(blueList).ToList();
+            //Set collisions to red, remove all from green and blue list
+            templist.ForEach(o => { o.colour = Color.Red; while (greenList.Remove(o)) ; while (blueList.Remove(o)) ; });
+            //Add the temp list to the red list (Collisions)
+            redList = new List<BouncingBalls>(redList.Union(templist));
+
+            //Clear the canvases
+            bgCanvas.Clear();
+            rCanvas.Clear();
+            //Add the counts to them
+            bgCanvas.AddText($"Blue : {blueList.Count} Green : {greenList.Count}", 40, Color.DodgerBlue);
+            rCanvas.AddText($"{redList.Count}", 50, Color.DodgerBlue);
+
+            for (int i = 0; i < blueList.Count; i++)
+            {
+                blueList[i].ShowBall(bgCanvas, i);
+            }
+            for (int i = 0; i < greenList.Count; i++)
+            {
+                greenList[i].ShowBall(bgCanvas, i);
+            }
+            for (int i = 0; i < redList.Count; i++)
+            {
+                redList[i].ShowBall(rCanvas, i);
+            }
+
+            bgCanvas.Render();
+            rCanvas.Render();
         }
     }
 }
