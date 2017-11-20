8fb9fa5cc2bc94b5a4fbaf905144f7ad25ea37f1
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-20 08:26:18 -0700

    Added gdi drawer

--- ICA/ICA14_NicW/ICA14_NicW/Block.cs
+++ ICA/ICA14_NicW/ICA14_NicW/Block.cs
@@ -9,14 +9,48 @@ namespace ICA14_NicW
 {
     class Block
     {
+        //The rectangle for the block
         protected RectangleF rect;
+        //Check if the block is outside the screen
         public bool Outside { get; private set; }
 
+        //Random number generator, shared by all blocks and derivatives
         protected static Random randNum = new Random();
 
+        //Constructor
         public Block(PointF location)
         {
-            rect = new RectangleF(location, );
+            rect = new RectangleF();
+            rect.X = location.X;
+            rect.Y = location.Y;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Block)) return false; //Object isn't a block, or null
+
+            Block temp = (Block)obj;
+
+            if(ReferenceEquals(this, temp)) //Are they the same rectangle? False if same rectangle
+            {
+                return false;
+            }
+            else
+            {
+                return this.rect.IntersectsWith(temp.rect); //Equal if rectangles overlap
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public virtual void Move(List<Block> inList)
+        {
+
+        }
+
+
     }
 }
