fa75bb47b64b1010288e1469f8cd5e27e3614338
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-20 08:12:18 -0700

    Added class

---
+++ ICA/ICA14_NicW/ICA14_NicW/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ICA14_NicW
+{
+    class Block
+    {
+        protected RectangleF rect;
+        public bool Outside { get; private set; }
+
+        protected static Random randNum = new Random();
+
+        public Block(PointF location)
+        {
+            rect = new RectangleF(location, );
+        }
+    }
+}
