35cbf6878cd4a47c7d81c530866d2cc1306106be
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-27 09:55:22 -0700

    Done ICA15

---
+++ ICA/ICA15_NicW/ICA15_NicW/Gate.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA15_NicW
+{
+    abstract class Gate
+    {
+        //Our input and output members
+        protected bool inputA;
+        protected bool inputB;
+        protected bool output;
+        //Public interface to read our members
+        public bool InputA { get { return inputA; } }
+        public bool InputB { get { return inputB; } }
+        public bool Output { get { return output; } }
+
+        //Set the values of A and B inputs
+        public void Set(bool A, bool B)
+        {
+            inputA = A;
+            inputB = B;
+        }
+
+        //latch will set the output, as determined by gate type and inputs
+        protected abstract void latch();
+        public void Latch() { latch(); }
+
+        //Name of gate type
+        protected abstract string name();
+        public string Name() { return name(); }
+
+        //Returns the truth table for the gate
+        public static string ToTable(Gate gate)
+        {
+            //We are building the string to output
+            StringBuilder sb = new StringBuilder();
+
+            //Start with the top line, inputs and output
+            sb.AppendLine($"A B  {gate.Name()}");
+
+            //Each input used and output generated
+            for(int a = 0; a < 2; a++)
+            {
+                for(int b = 0; b < 2; b++)
+                {
+                    gate.Set(a == 1, b == 1); //Set the inputs of the gate, where true is 1
+                    gate.latch();             //Sets the output
+                    sb.AppendLine($"{a} {b}  {Convert.ToInt32(gate.output)}"); //Adds the input/output line
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    class NANDGate : Gate
+    {
+        protected override string name()
+        {
+            return "NAND";
+        }
+
+        protected override void latch()
+        {
+            output = !(inputA & inputB);
+        }
+    }
+
+    class ANDGate : NANDGate
+    {
+        protected override string name()
+        {
+            return "AND";
+        }
+        protected override void latch()
+        {
+            base.latch();
+            output = !output;
+        }
+    }
+
+    class ORGate : Gate
+    {
+        protected override string name()
+        {
+            return "OR";
+        }
+        protected override void latch()
+        {
+            output = (inputA | inputB);
+        }
+    }
+
+    class XORGate : Gate
+    {
+        protected override string name()
+        {
+            return "XOR";
+        }
+        protected override void latch()
+        {
+            output = (inputA ^ inputB);
+        }
+    }
+}
---
+++ ICA/ICA15_NicW/ICA15_NicW/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA15_NicW
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Gate[] lGate = { new NANDGate(), new ANDGate(), new ORGate(), new XORGate()};
+            foreach(Gate g in lGate)
+            {
+                Console.WriteLine(Gate.ToTable(g));
+            }
+            Console.ReadLine();
+            Console.Clear();
+
+            //Main program
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"A B C D  O");
+
+            ANDGate and = new ANDGate();
+            NANDGate nand = new NANDGate();
+            ORGate or = new ORGate();
+            XORGate xor = new XORGate();
+
+            for(int a = 0; a < 2; a++)
+            {
+                for (int b = 0; b < 2; b++)
+                {
+                    for (int c = 0; c < 2; c++)
+                    {
+                        for (int d = 0; d < 2; d++)
+                        {
+                            //1
+                            and.Set(a == 1, a == 1);
+                            and.Latch();
+                            //2
+                            or.Set(a == 1, b == 1);
+                            or.Latch();
+                            //3
+                            nand.Set(b == 1, c == 1);
+                            nand.Latch();
+                            //4
+                            xor.Set(nand.Output, c == 1);
+                            xor.Latch();
+                            //5
+                            nand.Set(and.Output, or.Output);
+                            nand.Latch();
+                            //6
+                            or.Set(and.Output, nand.Output);
+                            //7
+                            and.Set(or.Output, xor.Output);
+                            or.Latch();
+                            and.Latch();
+                            //8
+                            xor.Set(xor.Output, d == 1);
+                            xor.Latch();
+                            //9
+                            and.Set(or.Output, and.Output);
+                            and.Latch();
+                            //10
+                            or.Set(and.Output, xor.Output);
+                            or.Latch();
+
+                            sb.AppendLine($"{a} {b} {c} {d}  {Convert.ToInt32(or.Output)}");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
