cf46f2c211a2976c48b98d2722a1dfc46b743a4e
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-29 15:42:55 -0700

    Had to make it an array.
    its dumb

--- ICA/ICA15_NicW/ICA15_NicW/Program.cs
+++ ICA/ICA15_NicW/ICA15_NicW/Program.cs
@@ -26,10 +26,7 @@ namespace ICA15_NicW
             StringBuilder sb = new StringBuilder();
             sb.AppendLine($"A B C D  O");
 
-            ANDGate and = new ANDGate();
-            NANDGate nand = new NANDGate();
-            ORGate or = new ORGate();
-            XORGate xor = new XORGate();
+            Gate[] circuit = {new ANDGate(), new ORGate(), new NANDGate(), new XORGate(), new NANDGate(), new ORGate(), new ANDGate(), new XORGate(), new ANDGate(), new ORGate()};
 
             for(int a = 0; a < 2; a++)
             {
@@ -40,37 +37,37 @@ namespace ICA15_NicW
                         for (int d = 0; d < 2; d++)
                         {
                             //1
-                            and.Set(a == 1, a == 1);
-                            and.Latch();
+                            circuit[0].Set(a == 1, a == 1);
+                            circuit[0].Latch();
                             //2
-                            or.Set(a == 1, b == 1);
-                            or.Latch();
+                            circuit[1].Set(a == 1, b == 1);
+                            circuit[1].Latch();
                             //3
-                            nand.Set(b == 1, c == 1);
-                            nand.Latch();
+                            circuit[2].Set(b == 1, c == 1);
+                            circuit[2].Latch();
                             //4
-                            xor.Set(nand.Output, c == 1);
-                            xor.Latch();
+                            circuit[3].Set(circuit[2].Output, c == 1);
+                            circuit[3].Latch();
                             //5
-                            nand.Set(and.Output, or.Output);
-                            nand.Latch();
+                            circuit[4].Set(circuit[0].Output, circuit[1].Output);
+                            circuit[4].Latch();
                             //6
-                            or.Set(and.Output, nand.Output);
+                            circuit[5].Set(circuit[0].Output, circuit[4].Output);
+                            circuit[5].Latch();
                             //7
-                            and.Set(or.Output, xor.Output);
-                            or.Latch();
-                            and.Latch();
+                            circuit[6].Set(circuit[1].Output, circuit[3].Output);
+                            circuit[6].Latch();
                             //8
-                            xor.Set(xor.Output, d == 1);
-                            xor.Latch();
+                            circuit[7].Set(circuit[3].Output, d == 1);
+                            circuit[7].Latch();
                             //9
-                            and.Set(or.Output, and.Output);
-                            and.Latch();
+                            circuit[8].Set(circuit[5].Output, circuit[6].Output);
+                            circuit[8].Latch();
                             //10
-                            or.Set(and.Output, xor.Output);
-                            or.Latch();
+                            circuit[9].Set(circuit[8].Output, circuit[7].Output);
+                            circuit[9].Latch();
 
-                            sb.AppendLine($"{a} {b} {c} {d}  {Convert.ToInt32(or.Output)}");
+                            sb.AppendLine($"{a} {b} {c} {d}  {Convert.ToInt32(circuit[9].Output)}");
                         }
                     }
                 }
---
+++ ICA/ICA16_NicW/ICA16_NicW/BaseShape.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+using MyDrawers;
+
+namespace ICA16_NicW
+{
+    public abstract class BaseShape
+    {
+        public static PictDrawer _canvas { get; set; }
+        public static Random _rnd { get; private set; }
+        private Point _pt { get; set; } //Center of shape
+        public Color _color { get; private set; } //Colour of shape
+        private double _dir = 0;    //Radians vector of direction
+        private double _vel = 5;    //Movement velocity
+
+        static BaseShape()
+        {
+            _rnd = new Random(0);
+        }
+        public BaseShape(Point pt, Color c)
+        {
+            _pt = pt;
+            _color = c;
+            //0 is right, then counter clockwise, in radians
+            _dir = _rnd.NextDouble() * 2 * Math.PI;
+        }
+
+        public Point Move() // NVI - public Move() 
+        {
+            return VirtualMove(); // invokes VirtualMove()
+        }
+        protected virtual Point VirtualMove()
+        {
+            // Adjust current direction by small random amount
+            _dir = _dir + (_rnd.NextDouble() * 0.8 - 0.4);
+            // Calculate new X, Y based on direction and velocity
+            int iNewX = _pt.X + (int)(_vel * Math.Cos(_dir));
+            int iNewY = _pt.Y - (int)(_vel * Math.Sin(_dir));
+            // Bounce by adding 90 degrees to direction
+            if (iNewX < 0 || iNewX >= _canvas.ScaledWidth)
+            {
+                _dir += Math.PI / 2;
+                iNewX = _pt.X;
+            }
+            if(iNewY < 0 || iNewY >= _canvas.ScaledHeight)
+            {
+                _dir += Math.PI / 2;
+                iNewY = _pt.Y;
+            }
+            _pt = new Point(iNewX, iNewY); // Save and return new Point
+            return _pt;
+        }
+
+        public void Paint() // NVI - public Paint()
+        {
+            VirtualPaint(); // invokes VirtualPaint()
+        }
+        protected abstract void VirtualPaint();
+    }
+}
